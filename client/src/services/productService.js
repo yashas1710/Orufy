069ce234eee117b58c105b55310e125320c68984
Yashas Sharma <yashas135790@gmail.com>
2025-12-22 11:41:48 +0530

    final commit

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${import.meta.env.VITE_API_URL}/api/products`;
-
+const API_URL = `${process.env.REACT_APP_API_URL}/api/products`;
 
 export const getProducts = async () => {
   const res = await axios.get(API_URL);
